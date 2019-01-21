5a9f12705946af57717b548d53c629c4baac7bd2
GroverTB <grover.jtb@gmail.com>
2019-01-21 15:03:58 -0500

    [ ADD ] appcenter android and ios

---
+++ ios/Pods/Target Support Files/Pods-reactNativeBoilerplate-tvOS/Pods-reactNativeBoilerplate-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reactNativeBoilerplate_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_reactNativeBoilerplate_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-reactNativeBoilerplate/Pods-reactNativeBoilerplate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_reactNativeBoilerplate : NSObject
+@end
+@implementation PodsDummy_Pods_reactNativeBoilerplate
+@end
